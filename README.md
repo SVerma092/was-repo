ee162241da96dd5de4014f78dc30ea2612949f55
sverma092 <sristiverma@kpmg.com>
2023-12-16 11:56:15 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # was-repo
+
+# create ssh key pair using cmd --> first key
+# public and private key
+# then whenever we create a new instance key should be assigned to that instance
+
+# create security group
--- terraform/core/vm.tf
+++ terraform/core/vm.tf
@@ -1,8 +1,10 @@
 resource "aws_instance" "web" {
-  ami           = "ami-0759f51a90924c166s"
+  ami           = "ami-0cd601a22ac9e6d79"
   instance_type = "t3.micro"
+  key_name = aws_key_pair.deployer.key_name
 
   tags = {
     Name = "HelloWorld"
   }
+  depends_on = [ aws_key_pair.deployer ]
 }
