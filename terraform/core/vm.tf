73dea622e3fced288a25f8fe61d45b972eb2266b
sverma092 <sristiverma@kpmg.com>
2023-12-20 15:26:06 +0530

    FMT

--- terraform/core/vm.tf
+++ terraform/core/vm.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "web" {
   ami           = "ami-0cd601a22ac9e6d79"
   instance_type = "t3.micro"
-  key_name = aws_key_pair.deployer.key_name
+  key_name      = aws_key_pair.deployer.key_name
 
   tags = {
     Name = "HelloWorld"
   }
-  depends_on = [ aws_key_pair.deployer ]
+  depends_on = [aws_key_pair.deployer]
 }
